0e5d6d23a1aaee705d6eb3eea06295f8249f9dfc
grzegorzpnk <grzegorzpnk@gmail.com>
2019-05-24 13:39:32 +0200

    "simple server version"

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, request
 
 app = Flask(__name__)
 
@@ -8,3 +8,19 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+@app.route('/success/<name>')
+def success(name):
+   return 'welcome %s' % name
+
+@app.route('/login',methods = ['POST', 'GET'])
+def login():
+   if request.method == 'POST':
+      user = request.form['nm']
+      return redirect(url_for('success', name=user))
+   else:
+      user = request.args.get('nm')
+      return redirect(url_for('success', name=user))
+
+if __name__ == '__main__':
+   app.run(debug=True)
